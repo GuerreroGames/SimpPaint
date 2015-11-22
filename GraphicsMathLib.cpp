959dbec1d51e73f166a06df832a3db6e84da21e2
Alexander Guerrero <guerreap@mcmaster.ca>
2015-11-22 18:51:25 -0500

    Made minor changes GraphicsMathLib

--- GraphicsMathLib.cpp
+++ GraphicsMathLib.cpp
@@ -4,12 +4,14 @@
 
 
 
-Point2D::Point2D(){
+Point2D::Point2D()
+{
 	x = 0.0;
 	y = 0.0;
 }
 
-Point2D::Point2D(float inX, float inY){
+Point2D::Point2D(float inX, float inY)
+{
 	x = inX;
 	y = inY;
 }
@@ -17,19 +19,22 @@ Point2D::Point2D(float inX, float inY){
 
 
 
-Vec2D::Vec2D(){
+Vec2D::Vec2D()
+{
 	x = 0.0;
 	y = 0.0;
 	magnitude = 0.0;
 }
 
-Vec2D::Vec2D(float inX, float inY, float mag){
+Vec2D::Vec2D(float inX, float inY, float mag)
+{
 	x = inX;
 	y = inY;
 	magnitude = mag;
 }
 
-float Vec2D::calcMag(){
+float Vec2D::calcMag()
+{
 	float mag = sqrt( pow(x, 2) + pow(y, 2) );
 	magnitude = mag;
 	return round(mag);
@@ -38,13 +43,15 @@ float Vec2D::calcMag(){
 
 
 
-Colour::Colour(){
+Colour::Colour()
+{
 	r = 0.0;
 	g = 0.0;
 	b = 0.0;
 }
 
-Colour::Colour(float red, float green, float blue){
+Colour::Colour(float red, float green, float blue)
+{
 	r = red;
 	g = green;
 	b = blue;
@@ -55,24 +62,28 @@ Colour::Colour(float red, float green, float blue){
 
 MathLib::MathLib(){}
 
-float MathLib::distBetween(Point2D p1, Point2D p2){
+float MathLib::distBetween(Point2D p1, Point2D p2)
+{
 	float dist = sqrt( pow((p2.x - p1.x), 2)
 					 + pow((p2.y - p1.y), 2) ); 
 	return dist;
 }
 
-float MathLib::fastDist(Point2D p1, Point2D p2){
+float MathLib::fastDist(Point2D p1, Point2D p2)
+{
 	float dist = ( (p2.x - p1.x) * (p2.x - p1.x) )
 			   + ( (p2.y - p1.y) * (p2.y - p1.y) );
 	return dist;
 }
 
-float MathLib::vecLength(Vec2D vec){
+float MathLib::vecLength(Vec2D vec)
+{
 	vec.calcMag();
 	return vec.magnitude;
 }
 
-Vec2D MathLib::normalize(Vec2D vec){
+Vec2D MathLib::normalize(Vec2D vec)
+{
 	float length = vec.calcMag();
 
 	float normX = vec.x / length;
@@ -85,7 +96,8 @@ Vec2D MathLib::normalize(Vec2D vec){
 	return normVec;
 }
 
-Vec2D MathLib::vectorMultiply(Vec2D vec, float scalar){
+Vec2D MathLib::vectorMultiply(Vec2D vec, float scalar)
+{
 	// v * S = (S * x, S * y)
 	float multX = vec.x * scalar;
 	float multY = vec.y * scalar;
@@ -96,7 +108,8 @@ Vec2D MathLib::vectorMultiply(Vec2D vec, float scalar){
 	return multVec;
 }
 
-Vec2D MathLib::createVector(Point2D first, Point2D second){
+Vec2D MathLib::createVector(Point2D first, Point2D second)
+{
 	// p1p2 = (x2 - x1, y2 - y1)
 	float x = second.x - first.x;
 	float y = second.y - first.y;
@@ -107,7 +120,8 @@ Vec2D MathLib::createVector(Point2D first, Point2D second){
 	return vec;
 }
 
-Point2D MathLib::movePoint(Point2D point, Vec2D vec){
+Point2D MathLib::movePoint(Point2D point, Vec2D vec)
+{
 	float x = point.x + vec.x;
 	float y = point.y + vec.y;
 	Point2D movedPoint = Point2D(x, y);
